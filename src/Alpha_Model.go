34ac06fd9b4de7bb9fcb42d1fba32391351ce1f1
MrCipher691 <ayush.ram691@gmail.com>
2020-09-05 19:30:44 +0530

    Fix

--- src/Alpha_Model.go
+++ src/Alpha_Model.go
@@ -14,29 +14,29 @@ type name struct {
 
 func main() {
 
-	read_input := bufio.NewScanner(os.Stdin)
+	readinput := bufio.NewScanner(os.Stdin)
 	fmt.Printf("\nPlease enter the filename containing a list of names: ")
-	read_input.Scan()
-	file_name := read_input.Text()
+	readinput.Scan()
+	filename := readinput.Text()
 
-	file, err := os.Open(file_name)
+	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Println("\nFile could not be opened. Please make sure you have the")
 		fmt.Println("correct filename and path before running again.")
 	}
 
-	name_slice := make([]name, 0)
+	nameslice := make([]name, 0)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		name_strings := strings.Fields(scanner.Text())
-		p := name{fname: name_strings[0], lname: name_strings[1]}
-		name_slice = append(name_slice, p)
+		namestrings := strings.Fields(scanner.Text())
+		p := name{fname: namestrings[0], lname: namestrings[1]}
+		nameslice = append(nameslice, p)
 	}
 
-	fmt.Println("\nHere are your list of names from", file_name)
+	fmt.Println("\nHere are your list of names from", filename)
 
-	for i := 0; i < len(name_slice); i++ {
-		fmt.Println(name_slice[i].fname, name_slice[i].lname)
+	for i := 0; i < len(nameslice); i++ {
+		fmt.Println(nameslice[i].fname, nameslice[i].lname)
 	}
 
 }
