6f503d2d6503890a4d9346748c056ae258b38909
Ayush Ram <ayush.ram691@gmail.com>
2020-11-02 19:12:33 +0530

    Final Comments

--- DMPT/src/Dmpt.go
+++ DMPT/src/Dmpt.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Prints stable matching for 4 men & women. Men are numbered as 0 to 3. Women are numbereed as 4 to 7.
+// Prints stable matching for 4 men & women. Men are numbered as 0 to 3. Women are numbered as 4 to 7.
 func stablemarriage(prefer [8][4]int) {
 	// Stores partner of women. This is our output array that stores pairing information. The value of wPartner[i] indicates the partner assigned to woman 4+i.
 	// Note that the woman numbers between 4 and 7. The value -1 indicates that (4+i)'th woman is free. Initialized as free.
@@ -101,7 +101,7 @@ func longGCD(x int, y int, x1 *int, x2 *int) int {
 	return x5
 }
 
-//Driver Function
+// Main/Driver Function
 func main() {
 	// Declaring all variables required for running the program
 	var ch, x, y, z, temp int
@@ -127,31 +127,45 @@ func main() {
 
 	switch ch {
 	case 1:
+		// Exectue stable marriage function
 		stablemarriage(prefer)
 	case 2:
+		// Taking 2 integer inputs to calculate their GCD
 		fmt.Printf("Enter 1st no.: ") // x = 8923794876682362
 		fmt.Scanf("%d", &x)
 		fmt.Printf("Enter 2nd no.: ") // y = 9384764854422
 		fmt.Scanf("%d", &y)
 
+		// Since function is designed in such a way that first variable has to be > or = to other one.
+		// This if-else case is used to decide which variable goes first.
 		if x > y {
+			// Since x>y, this function is executed.
 			z = euclid(x, y)
+			// Printing Solution.
 			fmt.Printf("GCD of %d & %d is %d\n", x, y, z)
 		} else {
+			// Since y>=x, this function is executed.
 			z = euclid(y, x)
+			// Printing Solution.
 			fmt.Printf("GCD of %d & %d is %d\n", x, y, z)
 		}
 	case 3:
+		// Taking n and m variables for pigeon hole algorithm formula.
 		fmt.Printf("Enter n; no. of pigeons: ") // n = 500
 		fmt.Scanf("%f", &n)
 		fmt.Printf("Enter m; no. of pigeonholes: ") // m = 45
 		fmt.Scanf("%f", &m)
 
+		// Pigeon Hole Algorithm Formula
 		ph = (((n - 1) / m) + 1)
+		// Converting float answer to int type, to get rid to decimal places.
 		temp = int(ph)
+		// Converting int back to float since Golang does not allow comparison of variable of 2 different data types.
 		phInt = float64(temp)
 
+		// Checking pigeon hole algorithms condition: n>m.
 		if n > m {
+			// This if-else is defined to round up values so that we get final integer answers.
 			if ph > phInt {
 				fmt.Printf("If %.2f pigeons are accommodated in %.2f pigeonholes and since %.2f>%.2f, then one of the pigeonhole must contain at least %.2f pigeons (Acc. to formula %.2f)\n", n, m, n, m, phInt+1, ph)
 			} else {
@@ -160,25 +174,38 @@ func main() {
 		}
 	case 4:
 		fmt.Printf("For multiplicative inverse to exist both the numbers have to be co-prime.\n")
+		// Taking 2 integer inputs to calculate multiplicative inverse
 		fmt.Printf("Enter 1st no.: ") //x = 392
 		fmt.Scanf("%d", &x)
 		fmt.Printf("Enter 2nd no.: ") //y = 27
 		fmt.Scanf("%d", &y)
 
+		// Since function is designed in such a way that first variable has to be > or = to other one.
+		// This if-else case is used to decide which variable goes first.
 		if x > y {
+			// Since x>y, this function is executed.
 			z = euclid(x, y)
+			// Printing Solution.
 			fmt.Printf("GCD of %d & %d is %d\n", x, y, z)
 		} else {
+			// Since y>=x, this function is executed.
 			z = euclid(y, x)
+			// Printing Solution.
 			fmt.Printf("GCD of %d & %d is %d\n", x, y, z)
 		}
 
+		// Condition to see if multiplicative inverse; the two numbers should be co-prime.
 		if z != 1 {
+			// The numbers are not co-prime, hence their solution does not exist.
 			fmt.Printf("! -- NO SOLUTION -- !\n")
 		} else {
+			// Since function is designed in such a way that first variable has to be > or = to other one.
+			// This if-else case is used to decide which variable goes first.
 			if x > y {
+				// Printing solution.
 				fmt.Printf("Solution: %d\n", multipliInverse(y, x))
 			} else {
+				// Printing solution.
 				fmt.Printf("Solution: %d\n", multipliInverse(x, y))
 			}
 		}
